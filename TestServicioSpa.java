8f95e5289d2fd79620094f887ba90bf58ef4ecb7
Miracles-student <126127415+Miracles-student@users.noreply.github.com>
2023-05-11 10:28:21 -0300

    Update TestServicioSpa.java

--- TestServicioSpa.java
+++ TestServicioSpa.java
@@ -1,12 +1,15 @@
 
 public class TestServicioSpa {
     public static void main(String[] args) {
+        // creamos los objetos
         ServicioSpa servicioSpa1 = new ServicioSpa();
         ServicioSpa servicioSpa2 = new ServicioSpa();
+        // se intancian los objetos
         servicioSpa1.setDescripcion(Facial);
         servicioSpa1.setPrecio(150.20);
         servicioSpa2.setDescripcion(Masaje);
         servicioSpa2.setPrecio(340.00);
+        // se muestran los valores
         System.out.println("Primer servicio");
         System.out.println(servicioSpa1.getDescripcion() + " " + servicioSpa1.getPrecio());
         System.out.println("Segundo servicio");
